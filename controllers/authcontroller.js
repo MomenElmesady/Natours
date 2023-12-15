8e242c417fb34d9cad0aa39894eb7dbe19343431
Momen Ashraf <momenelmesady4803@gmail.com>
2023-12-15 15:29:44 +0200

    add comment

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -76,6 +76,7 @@ exports.protect = catchAsync(async (req, res, next) => {
     let token;
     if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
         token = req.headers.authorization.split(" ")[1]
+        // for development 
         if (token === "false"){
             token = false 
         }
